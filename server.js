c0c4552167c407079eb0eddb87b137766f7a6589
theonlyriad <riadmesbah608@email.com>
2026-06-26 23:19:10 +0100

    adding the email login

--- server.js
+++ server.js
@@ -158,7 +158,9 @@ app.post("/login", async (req, res) => {
         const { username, password } = req.body;
 
         const result = await pool.query(
-            "SELECT * FROM users WHERE username = $1",
+            `SELECT * FROM users
+   WHERE LOWER(username) = LOWER($1)
+   OR LOWER(email) = LOWER($1)`,
             [username]
         );
 
